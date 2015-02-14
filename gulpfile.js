b004f595aaa01570b6066994d89165d44943b7cb
Adam Campbell <adam@hotmeteor.com>
2015-02-14 08:30:01 -0600

    Content, CSS, meta updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-
+var critical = require('critical');
 var wiredep = require('wiredep').stream;
 var handleErrors = require('./gulp/handleErrors');
 
@@ -18,10 +18,12 @@ var paths = {
     build: 'dist'
 };
 
+var buildFile = 'index.html';
+
 // load options
 var bumpType = $.util.env.type || 'patch';
 
-gulp.task('styles', function () {
+gulp.task('styles', function() {
     $.util.log('Rebuilding application styles');
 
     return gulp.src(paths.dev + '/scss/*.scss')
@@ -32,15 +34,52 @@ gulp.task('styles', function () {
             errLogToConsole: true,
             onError: browserSync.notify
         }))
-        .pipe($.autoprefixer(['last 5 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+        .pipe($.autoprefixer(['last 5 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
         .pipe(gulp.dest(paths.dev + '/styles'))
-        .pipe($.size({showFiles: true}))
+        .pipe($.size({
+            showFiles: true
+        }))
         .pipe($.filter('**/*.css'))
-        .pipe(reload({stream: true}))
+        .pipe(reload({
+            stream: true
+        }))
         .pipe($.notify('CSS compiled and autoprefixed'));
 });
 
-gulp.task('scripts', function () {
+// Copy our site styles to a site.css file
+// for async loading later
+gulp.task('copystyles', function() {
+    return gulp.src(['dist/styles/main.css'])
+        .pipe($.rename({
+            basename: "site"
+        }))
+        .pipe(gulp.dest('dist/styles'));
+});
+
+
+gulp.task('critical', ['copystyles'], function() {
+
+    critical.generate({
+        base: paths.build + '/',
+        src: buildFile,
+        dest: 'styles/site.css',
+        width: 320,
+        height: 480,
+        minify: true
+    }, function(err, output) {
+        critical.inline({
+            base: paths.build + '/',
+            src: buildFile,
+            dest: buildFile,
+            minify: true
+        });
+    });
+
+});
+
+gulp.task('scripts', function() {
     return gulp.src(paths.dev + '/scripts/**/*.js')
         .pipe($.jshint())
         .pipe($.jshint.reporter(require('jshint-stylish')))
@@ -49,30 +88,29 @@ gulp.task('scripts', function () {
         .pipe($.notify('JS hinted'));
 });
 
-gulp.task('html', ['styles'], function () {
+gulp.task('html', ['styles'], function() {
     var jsFilter = $.filter('**/*.js');
     var cssFilter = $.filter('**/*.css');
     var assets = $.useref.assets();
 
-    return gulp.src(paths.dev + '/index.html')
+    return gulp.src(paths.dev + '/' + buildFile)
         .pipe(assets)
         .pipe(jsFilter)
-        .pipe($.uglify({preserveComments: $.uglifySaveLicense}))
+        .pipe($.uglify({
+            preserveComments: $.uglifySaveLicense
+        }))
         .pipe(jsFilter.restore())
         .pipe(cssFilter)
-        // .pipe($.replace('bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap','fonts'))
-        // .pipe($.combineMediaQueries())
         .pipe($.csso())
         .pipe(cssFilter.restore())
         .pipe(assets.restore())
         .pipe($.useref())
-        // .pipe($.revReplace())
         .pipe(gulp.dest(paths.build))
         .pipe($.size())
         .pipe($.notify('CSS and JS concatted and minified'));
 });
 
-gulp.task('images', function () {
+gulp.task('images', function() {
     return gulp.src(paths.dev + '/images/**/*')
         .pipe($.cache($.imagemin({
             optimizationLevel: 3,
@@ -84,7 +122,7 @@ gulp.task('images', function () {
         .pipe($.notify('Images minified'));
 });
 
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
     return gulp.src($.mainBowerFiles())
         .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
         .pipe($.flatten())
@@ -92,7 +130,14 @@ gulp.task('fonts', function () {
         .pipe($.size());
 });
 
-gulp.task('serve', function () {
+gulp.task('extras', function() {
+    return gulp.src(['app/*.*', '!app/*.html'], {
+            dot: true
+        })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', function() {
     browserSync({
         server: {
             baseDir: paths.dev
@@ -100,7 +145,7 @@ gulp.task('serve', function () {
     });
 });
 
-gulp.task('watch', ['styles', 'serve'], function () {
+gulp.task('watch', ['styles', 'serve'], function() {
 
     // watch for changes to reload
     gulp.watch([
@@ -113,21 +158,21 @@ gulp.task('watch', ['styles', 'serve'], function () {
     gulp.watch(paths.dev + '/scss/**/*', ['styles']);
 });
 
-gulp.task('build', ['html', 'images', 'fonts']);
+gulp.task('build', ['html', 'critical', 'images', 'fonts', 'extras']);
 
-gulp.task('bower', function () {
+gulp.task('bower', function() {
     gulp.src(paths.dev + '/build.html')
         .pipe(wiredep({
-            exclude: [
-
-            ]
+            exclude: []
         }))
         .pipe(gulp.dest(paths.dev));
 });
 
 // Update bower, component, npm at once:
-gulp.task('bump', function(){
+gulp.task('bump', function() {
     gulp.src(['./bower.json', './package.json'])
-        .pipe($.bump({ type: bumpType }))
+        .pipe($.bump({
+            type: bumpType
+        }))
         .pipe(gulp.dest('./'));
 });
